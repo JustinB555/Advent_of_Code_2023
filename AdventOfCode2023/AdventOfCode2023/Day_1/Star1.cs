89a35ecca1139de5549cbf5323b560b7253eb139
JustinB555 <justin.b208@gmail.com>
2023-12-02 13:09:51 -0700

    Update Day 2

--- AdventOfCode2023/AdventOfCode2023/Day_1/Star1.cs
+++ AdventOfCode2023/AdventOfCode2023/Day_1/Star1.cs
@@ -11,18 +11,18 @@ namespace AdventOfCode2023.Day_1
 {
 	internal class Star1
 	{
-		public List<string> Data { get; set; }
+		//static void Main ()
+		//{
+		//	int results = CollectCalibrationData();
 
-		static void Main ()
-		{
-			CollectCalibrationData();
-		}
+		//	Console.WriteLine(results);
+		//}
 
-		static List<string> CollectCalibrationData()
+		static int CollectCalibrationData()
 		{
-			List<string> dt = new List<string>();
 			string filePath = "C:/Github/Advent_of_Code_2023/AdventOfCode2023/AdventOfCode2023/files/Trebuchet_Calibration.txt";
 			string fullText = "";
+			int totalSum = 0;
 
 			if (File.Exists(filePath))
 			{
@@ -31,17 +31,75 @@ namespace AdventOfCode2023.Day_1
 					while (!sr.EndOfStream)
 					{
 						fullText = sr.ReadToEnd().ToString();
-						string output = Regex.Replace(fullText, "[^0-9.\n]", "");
+						string convertingOne = ConvertText(fullText);
+						string output = Regex.Replace(convertingOne, "[^0-9.\n]", "");
 						string[] rows = output.Split('\n');
+						int count = 0;
 						for (int i = 0; i < rows.Length; i++)
 						{
-							
+							var number = "";
+							var single = "";
+							var current = rows[i];
+							if (rows[i].Length > 1)
+							{
+								number = $"{rows[i].Substring(0, 1)}{rows[i].Substring(rows[i].Length - 1)}";
+								totalSum += Convert.ToInt32(number);
+							}
+							else
+							{
+								single = $"{rows[i]}{rows[i]}";
+								totalSum += Convert.ToInt32(single);
+							}
+							var conditional = number != "" ? number : single;
+							Console.WriteLine($"{count} | Rows: {current} | String: {conditional} | Total: {totalSum}");
+							count++;
 						}
 					}
 				}
 			}
 
-			return dt;
+			return totalSum;
+		}
+
+		private static string ConvertText(string text)
+		{
+			string ret = text;
+			Dictionary<string, string> formatText = new Dictionary<string, string>
+			{
+				{"oneight", "oneeight"},
+				{"twone", "twoone"},
+				{"threeight", "threeeight"},
+				{"fiveight", "fiveeight"},
+				{"sevenine", "sevennine"},
+				{"eightwo", "eighttwo"},
+				{"eighthree", "eightthree"},
+				{"nineight", "nineeight"}
+			};
+
+			Dictionary<string, int> convertText = new Dictionary<string, int>
+			{
+				{"one", 1},
+				{"two", 2},
+				{"three", 3},
+				{"four", 4},
+				{"five", 5},
+				{"six", 6},
+				{"seven", 7},
+				{"eight", 8},
+				{"nine", 9}
+			};
+
+			foreach (KeyValuePair<string, string> ft in  formatText)
+			{
+				ret = Regex.Replace(ret, ft.Key, ft.Value);
+			}
+
+			foreach (KeyValuePair<string, int> ct in convertText)
+			{
+				ret = Regex.Replace(ret, ct.Key, ct.Value.ToString());
+			}
+
+			return ret;
 		}
 	}
 }
---
+++ AdventOfCode2023/AdventOfCode2023/Day_2/Cube_Conundrum.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Day_2
+{
+	internal class Cube_Conundrum
+	{
+		static void Main()
+		{
+			int restult = CubeGames(false);
+		}
+
+		static int CubeGames(bool puzzle1)
+		{
+			string filePath = "C:\\Github\\Advent_of_Code_2023\\AdventOfCode2023\\AdventOfCode2023\\files\\Cube_Conundrum.txt";
+			string fullText = "";
+			int gameTotal = 0;
+
+			if (File.Exists(filePath))
+			{
+				using (StreamReader sr = new StreamReader(filePath))
+				{
+					while (!sr.EndOfStream)
+					{
+						fullText = sr.ReadToEnd().ToString();
+						string[] rows = fullText.Split('\n');
+						if (puzzle1)
+						{
+							gameTotal = PossibleGames(rows);
+						}
+						else
+						{
+							gameTotal = FewestPossibleCubes(rows);
+						}
+					}
+				}
+			}
+
+			return gameTotal;
+		}
+
+		static int PossibleGames(string[] rows)
+		{
+			int ret = 0;
+
+			foreach (string row in rows)
+			{
+				bool breakout = false;
+				string[] formattingGame = row.Split(':');
+				string cleanGameNum = formattingGame[0].Replace("Game ", "");
+				int gameNum = Convert.ToInt32(cleanGameNum);
+
+				string[] formattingSet = formattingGame[1].Split(';');
+				foreach (string set in formattingSet)
+				{
+					string[] pull = set.Split(',');
+					foreach (string p in pull)
+					{
+						int num = 0;
+
+						if (p.Contains("red"))
+						{
+							num = Convert.ToInt32(p.Replace(" red", "").Trim());
+							if (num > 12)
+							{
+								breakout = true;
+								break;
+							}
+						}
+						else if (p.Contains("green"))
+						{
+							num = Convert.ToInt32(p.Replace(" green", "").Trim());
+							if (num > 13)
+							{
+								breakout = true;
+								break;
+							}
+
+						}
+						else if (p.Contains("blue"))
+						{
+							num = Convert.ToInt32(p.Replace(" blue", "").Trim());
+							if (num > 14)
+							{
+								breakout = true;
+								break;
+							}
+						}
+					}
+
+					if (breakout)
+					{
+						break;
+					}
+				}
+
+				if (!breakout)
+				{
+					ret += gameNum;
+				}
+			}
+
+			return ret;
+		}
+
+		static int FewestPossibleCubes(string[] rows)
+		{
+			int ret = 0;
+
+			foreach (string row in rows)
+			{
+				string[] formattingGame = row.Split(':');
+				string[] formattingSet = formattingGame[1].Split(';');
+				int red = 0;
+				int green = 0;
+				int blue = 0;
+
+				foreach (string set in formattingSet)
+				{
+					string[] pull = set.Split(',');
+					foreach (string p in pull)
+					{
+						int num = 0;
+
+						if (p.Contains("red"))
+						{
+							num = Convert.ToInt32(p.Replace(" red", "").Trim());
+							if (num > red)
+							{
+								red = num;
+							}
+						}
+						else if (p.Contains("green"))
+						{
+							num = Convert.ToInt32(p.Replace(" green", "").Trim());
+							if (num > green)
+							{
+								green = num;
+							}
+
+						}
+						else if (p.Contains("blue"))
+						{
+							num = Convert.ToInt32(p.Replace(" blue", "").Trim());
+							if (num > blue)
+							{
+								blue = num;
+							}
+						}
+					}
+				}
+			
+				ret += red * green * blue;
+			}
+
+			return ret;
+		}
+
+	}
+}
